3b1600600cd9b8dc5e36ae5f8b6b49569de3619c
Pratyush Solanki <vikash1stmay@hotmail.com>
2023-02-21 16:22:13 +0530

    Got a new idea so there were some changes

--- main.ps1
+++ main.ps1
@@ -1,5 +1,1 @@
-winget.exe import -i .\export_list.json
-$userPath = C:\Users\"$env:USERNAME"\StandaloneApps2
-mkdir $userPath
-
-Set-Location $userPath
+# winget.exe import -i .\export_list.json --accept-source-agreements  --verbose
